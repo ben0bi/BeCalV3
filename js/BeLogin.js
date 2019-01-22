11c7c961576b34133e7e92cbd265c59c64e80d83
FUCKUP THE ALMIGHTY <FUCKUP@submarine.sea>
2019-01-22 03:25:45 +0100

    GIT don't SVN all the time you fucker.

--- js/BeLogin.js
+++ js/BeLogin.js
@@ -11,16 +11,38 @@ function showLoginWindow(contentIDorClass)
 	inputName.attr('placeholder', 'Name');
 	var inputPW = jQuery.getNewElement('input', 'BeCal_input_login_pw','login_input');
 	inputPW.attr('placeholder', 'Password');
-	var linkSend = jQuery.getNewJSButton('<nobr>LOG IN</nobr>','sendLoginData','BeCal_button_login_send','btn');
+
+	var linkNormalSend = jQuery.getNewJSButton('<nobr>LOG IN</nobr>','sendLoginData()','BeCal_button_login_send','btn');
+
+/* New system stuff */
+	
+	var firstUserMessage = jQuery.getNewDiv('This is a <span class="green">fresh system</span> with <span class="red">no user</span>.<br />You need to <span class="green">create</span> a <span class="green">root user</span> with all privileges.','','login_message');
+	var redoPW = jQuery.getNewElement('input', 'BeCal_input_login_pw2','login_input');
+	redoPW.attr('placeholder', 'PW again');	
+	var linkAddRootUser = jQuery.getNewJSButton('<nobr>Install ROOT</nobr>', 'BeLogin.createRootUser()', 'BeCal_button_login_createRoot','btn');
+	
+/* endof new system stuff. */	
 	
 	var content = jQuery.getNewDiv('','BeCal_WINDOW_login','window')
 	//var el = jQuery.getNewDiv('','','positiveCenter');
 	
+
+// standard login
+/*	$(content).append(inputName);
+	$(content).append(inputPW);
+	$(content).append(linkNormalSend);
+*/
+
+// root user login
+	//$(content).append(firstUserMessage);
 	$(content).append(inputName);
 	$(content).append(inputPW);
-	$(content).append(linkSend);
-	
+	$(content).append(redoPW);
+	$(content).append(linkAddRootUser);
+
+
 	//$(el).append(content);
+	jQuery.appendElementTo(contentIDorClass,firstUserMessage);
 	
 	jQuery.appendElementTo(contentIDorClass,content);
 }
